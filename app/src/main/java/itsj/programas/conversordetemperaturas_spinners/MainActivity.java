42fce825d3e73ae8e33e534acb82f2458db26eb2
Cristofer32513 <cristofer32513@gmail.com>
2019-05-10 22:17:41 -0500

    Se instanciaron las variables.

--- app/src/main/java/itsj/programas/conversordetemperaturas_spinners/MainActivity.java
+++ app/src/main/java/itsj/programas/conversordetemperaturas_spinners/MainActivity.java
@@ -2,8 +2,12 @@ package itsj.programas.conversordetemperaturas_spinners;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -19,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerEntrada=findViewById(R.id.spinner_entrada);
+
+        spinnerSalida=findViewById(R.id.spinner_salida);
+
+        cajaEntrada=findViewById(R.id.caja_entrada);
+        
+        cajaSalida=findViewById(R.id.caja_salida);
+
+        ArrayAdapter adaptador=ArrayAdapter.createFromResource(this, R.array.spiner_entrada, android.R.layout.simple_spinner_item);
+        spinnerEntrada.setAdapter(adaptador);
+
+        ArrayAdapter adaptador2=ArrayAdapter.createFromResource(this, R.array.entrada_no_seleccionada, android.R.layout.simple_spinner_item);
+        spinnerSalida.setAdapter(adaptador2);
     }
 }
