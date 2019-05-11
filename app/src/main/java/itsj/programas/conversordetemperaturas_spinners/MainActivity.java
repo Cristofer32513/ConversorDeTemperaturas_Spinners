1047ae98d83e741b058045232e1147929a00eba8
Cristofer32513 <cristofer32513@gmail.com>
2019-05-10 22:26:52 -0500

    Se agrego el OnItemSelectedListener a los spinners de entrada y salida junto con el codigo para verificar que item esta seleccionado de cada spinner.

--- app/src/main/java/itsj/programas/conversordetemperaturas_spinners/MainActivity.java
+++ app/src/main/java/itsj/programas/conversordetemperaturas_spinners/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnKeyListener {
 
     Spinner spinnerEntrada, spinnerSalida;
     EditText cajaEntrada, cajaSalida;
@@ -25,11 +26,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinnerEntrada=findViewById(R.id.spinner_entrada);
+        spinnerEntrada.setOnItemSelectedListener(this);
 
         spinnerSalida=findViewById(R.id.spinner_salida);
+        spinnerSalida.setOnItemSelectedListener(this);
 
         cajaEntrada=findViewById(R.id.caja_entrada);
-        
+
         cajaSalida=findViewById(R.id.caja_salida);
 
         ArrayAdapter adaptador=ArrayAdapter.createFromResource(this, R.array.spiner_entrada, android.R.layout.simple_spinner_item);
@@ -38,4 +41,58 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adaptador2=ArrayAdapter.createFromResource(this, R.array.entrada_no_seleccionada, android.R.layout.simple_spinner_item);
         spinnerSalida.setAdapter(adaptador2);
     }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(parent.getId()==R.id.spinner_entrada) {
+            ArrayAdapter adaptador=ArrayAdapter.createFromResource(this, R.array.entrada_no_seleccionada, android.R.layout.simple_spinner_item);
+            if(spinnerEntrada.getSelectedItemPosition()==0) {
+                adaptador = ArrayAdapter.createFromResource(this, R.array.entrada_no_seleccionada, android.R.layout.simple_spinner_item);
+                spinnerSalida.setEnabled(false);
+            }
+            else if(spinnerEntrada.getSelectedItemPosition()==1) {
+                adaptador = ArrayAdapter.createFromResource(this, R.array.entrada_c_seleccionada, android.R.layout.simple_spinner_item);
+                spinnerSalida.setEnabled(true);
+            }
+            else if(spinnerEntrada.getSelectedItemPosition()==2) {
+                adaptador = ArrayAdapter.createFromResource(this, R.array.entrada_f_seleccionada, android.R.layout.simple_spinner_item);
+                spinnerSalida.setEnabled(true);
+            }
+            else if(spinnerEntrada.getSelectedItemPosition()==3) {
+                adaptador = ArrayAdapter.createFromResource(this, R.array.entrada_k_seleccionada, android.R.layout.simple_spinner_item);
+                spinnerSalida.setEnabled(true);
+            }
+            else if(spinnerEntrada.getSelectedItemPosition()==4) {
+                adaptador = ArrayAdapter.createFromResource(this, R.array.entrada_r_seleccionada, android.R.layout.simple_spinner_item);
+                spinnerSalida.setEnabled(true);
+            }
+
+            spinnerSalida.setAdapter(adaptador);
+        }
+        else if(parent.getId()==R.id.spinner_salida){
+            if(!cajaEntrada.getText().toString().equals("")){
+                if(cajaEntrada.getText().toString().equals("."))
+                    cajaEntrada.setText("0.");
+                tempEntrada=Double.parseDouble(cajaEntrada.getText().toString());
+                tempSalida=0;
+
+                if(spinnerSalida.getSelectedItem()==null)
+                    cajaSalida.setText("");
+                else{
+                    if(spinnerSalida.getSelectedItemPosition()==0)
+                        cajaSalida.setText("");
+                    else
+                        mostrarResultado();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 }
